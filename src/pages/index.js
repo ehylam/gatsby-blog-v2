c693e9db7ac7d30225c614b68b616ffc0f912040
Eric Lam <ehylam.p@gmail.com>
2020-06-16 18:10:28 +1000

    added title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 // import { Link } from "gatsby"
 
+import SEO from '../components/seo'
 import Hero from '../components/Hero/Hero'
 import PostFeed from '../components/PostFeed/PostFeed'
 import Layout from "../components/layout"
@@ -8,6 +9,7 @@ import Layout from "../components/layout"
 const IndexPage = ({data}) => {
   return (
   <Layout>
+      <SEO title={"I know that I don't know what I'm doing.."}/>
       <Hero/>
       <PostFeed />
   </Layout>
