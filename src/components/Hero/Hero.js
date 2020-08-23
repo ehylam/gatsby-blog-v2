a539a2e6bc63388c1e4c86ee938fe2728814e603
Eric Lam <ehylam.p@gmail.com>
2020-08-23 16:17:32 +1000

    added some fun gsap animation to hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,6 +6,32 @@ import Image from '../image'
 import BackgroundImage from 'gatsby-background-image'
 import { useStaticQuery, graphql } from 'gatsby'
 
+import { gsap } from "gsap"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+if (typeof window !== "undefined") {
+  gsap.registerPlugin(ScrollTrigger);
+}
+
+document.querySelector('.hero').addEventListener('mousemove',heroTransition);
+
+function heroTransition(e) {
+  const { offsetX, offsetY, target } = e;
+  const { clientWidth, clientHeight } = target;
+
+  const xPos = offsetX / clientWidth - 0.5;
+  const yPos = offsetY / clientHeight - 0.5;
+
+  gsap.to('.hero .bg', {
+    duration: 1.5,
+    x: xPos * 20,
+    y: yPos * 25,
+    rotationY: xPos * 30,
+    rotationX: yPos * 15
+  })
+}
+
+
 
 const Hero = () => {
   const data = useStaticQuery(graphql`
